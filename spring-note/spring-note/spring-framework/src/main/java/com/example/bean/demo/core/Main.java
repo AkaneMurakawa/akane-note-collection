0673e107c3fc6d742f6776741e9ca34685ee86b2
AkaneMurakawa <chenshjing@gmail.com>
2020-09-14 21:59:30 +0800

    add environment property test

--- spring-note/spring-note/spring-framework/src/main/java/com/example/bean/demo/core/Main.java
+++ spring-note/spring-note/spring-framework/src/main/java/com/example/bean/demo/core/Main.java
@@ -6,8 +6,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
     public static void main(String[] args) {
+
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/spring-core.xml");
         SimpleBean simpleBean = (SimpleBean) applicationContext.getBean("simpleBean");
         simpleBean.send();
+
+//        System.setProperty("path", "classpath");
+//        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("${path}:/META-INF/spring-core.xml");
     }
 }
