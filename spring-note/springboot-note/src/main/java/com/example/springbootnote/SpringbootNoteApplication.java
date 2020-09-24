c0e83bdd5264a18fc1380040d621379bb36f0c5d
AkaneMurakawa <chenshjing@gmail.com>
2020-09-24 19:19:07 +0800

    add redis cluster

--- spring-note/springboot-note/src/main/java/com/example/springbootnote/SpringbootNoteApplication.java
+++ spring-note/springboot-note/src/main/java/com/example/springbootnote/SpringbootNoteApplication.java
@@ -1,16 +1,26 @@
 package com.example.springbootnote;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringbootNoteApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication application = new SpringApplication(SpringbootNoteApplication.class);
-		//application.run(args);
+		// 方式一
+		SpringApplication application = new SpringApplication(SpringbootNoteApplication.class);
+		application.run(args);
 
-		SpringApplication.run(SpringbootNoteApplication.class, args);
+		// 方式二
+		//new SpringApplicationBuilder(SpringbootNoteApplication.class)
+		//		.web(WebApplicationType.SERVLET)
+		//		.run();
+
+		// 方式三
+		//SpringApplication.run(SpringbootNoteApplication.class, args);
 	}
 
 }
