878f7d07aa5cc240195d206f9f5070108d486a22
AkaneMurakawa <chenshjing@gmail.com>
2020-11-18 00:34:55 +0800

    测试

---
+++ jvm-note/src/main/java/com/example/TestPlus.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class TestPlus {
+
+    public static void main(String[] args) {
+        int i = 2;
+        i = i++; // 2
+//        i = ++i; // 3
+        System.out.println(i);
+    }
+}
