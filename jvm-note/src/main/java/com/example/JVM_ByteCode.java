15f6b5e16577f94a6b71a81b156c6cc3287b4d4b
AkaneMurakawa <chenshjing@gmail.com>
2020-11-19 00:15:00 +0800

    字节码分析

--- jvm-note/src/main/java/com/example/JVM_ByteCode.java
+++ jvm-note/src/main/java/com/example/JVM_ByteCode.java
@@ -1,4 +1,14 @@
 package com.example;
 
 public class JVM_ByteCode {
+
+
+    public static void main(String[] args) {
+        JVM_ByteCode jvm_byteCode = new JVM_ByteCode();
+        jvm_byteCode.sum(10);
+    }
+
+    public int sum(int i){
+        return i + 1;
+    }
 }
